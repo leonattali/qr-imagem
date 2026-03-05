a06c989ae304520c5ef57486dedf61c9519a4230
l/nattali <leoassis1000@gmail.com>
2026-03-05 17:56:06 +0000

    novotesteqrc

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const qrDiv = document.getElementById("qrcode");
 qrDiv.innerHTML = "";
 
 new QRCode(qrDiv, {
-text: "https://leonattali.github.io/qr-imagem",
+text: "https://leonattali.github.io/qr-imagem/textov2.html",
 width: 200,
 height: 200
 });
